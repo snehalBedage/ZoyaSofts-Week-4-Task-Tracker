a0f5069c4bba597d20450ed072f90cf1fe3c8635
Snehal Bedage <snehalbedage3630@gmail.com>
2026-09-19 19:45:19 +0530

    Fix Render allowed host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,10 +14,11 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "task-tracker-mypk.onrender.com",
+]
 
 
 # Application definition
