1ef867437531c4e0bc385d1974514962d38c453e
Snehal Bedage <snehalbedage3630@gmail.com>
2026-09-19 20:25:52 +0530

    Connect React frontend to deployed API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = "http://127.0.0.1:8000/tasks/";
-
+const API_URL = import.meta.env.VITE_API_URL;
 function App() {
   const [tasks, setTasks] = useState([]);
 
